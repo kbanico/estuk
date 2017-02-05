f9fb8203819a16915f74092680bf81ddb99ac2fa
ken <code4uguam@gmail.com>
2017-02-05 10:38:54 +0000

    make first sale

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   get "dashboard", to: "pages#dashboard"
   #Can also use
   # get 'dashboard' => 'pages#dashboard'
+  
+  post "/buy/:slug", to: "transactions#create", as: :buy
+  get "/pickup/:guid", to: "transactions#pickup", as: :pickup
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
