d7ec53c54742d193bb4c30501ed1dce13f444c14
ken <code4uguam@gmail.com>
2017-02-05 09:51:11 +0000

    add sale model

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,8 +4,12 @@ class Book < ApplicationRecord
   friendly_id :name, use: :slugged
   
   belongs_to :user
+  has_many :sales
   has_attached_file :image
   has_attached_file :resource
+  
+  validates_numericality_of :price,
+  greater_than: 49, message: "Must be at least 50 cents"
   
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   validates_attachment :resource, :content_type => { :content_type => %w(application/pdf application/msword application/vnd.openxmlformats-officedocument.wordprocessingml.document) }
